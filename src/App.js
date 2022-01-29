05853edf61a74c8accc017602e46ebacf8971e8a
konsent <konsent2@gmail.com>
2022-01-29 18:09:19 +0900

    -

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
 import Button from "./Button";
 import styles from "./App.module.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [Todo, setTodo] = useState("");
+  const [Todos, setTodos] = useState([]);
+  const onChange = (event) => setTodo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (Todo === "") {
+      return;
+    }
+
+    setTodos((currentArray) => [Todo, ...currentArray]);
+    setTodo("");
+  };
+  console.log(Todos);
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome Back</h1>
-      <Button text={"Continue!"} />
+      <h1> My To-dos ({Todos.length}개)</h1>
+      <form onSubmit={onSubmit}>
+        <input
+          onChange={onChange}
+          value={Todo}
+          type="text"
+          placeholder="Write your Todo"
+        ></input>
+        <button>Add To do</button>
+      </form>
     </div>
   );
 }
