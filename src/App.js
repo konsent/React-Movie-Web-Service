b13edb7dce8678b9e9814673f3c7e9311aca6f6c
konsent <konsent2@gmail.com>
2022-01-30 09:50:34 +0900

    Coin Selector

--- src/App.js
+++ src/App.js
@@ -3,32 +3,44 @@ import styles from "./App.module.css";
 import { useState, useEffect } from "react";
 
 function App() {
-  const [Todo, setTodo] = useState("");
-  const [Todos, setTodos] = useState([]);
-  const onChange = (event) => setTodo(event.target.value);
-  const onSubmit = (event) => {
-    event.preventDefault();
-    if (Todo === "") {
-      return;
-    }
+  const [loading, setLoading] = useState(true);
+  const [coins, setCoins] = useState([]);
+  const [selectedCoin, setSeletedCoin] = useState("");
+  const [myMoney, setMyMoney] = useState(0);
 
-    setTodos((currentArray) => [Todo, ...currentArray]);
-    setTodo("");
+  const onSelectCoin = (event) => {
+    setSeletedCoin(event.target.value);
   };
-  console.log(Todos);
 
+  const putMyMoney = (event) => {
+    setMyMoney(event.target.value);
+  };
+
+  useEffect(() => {
+    fetch("https://api.coinpaprika.com/v1/tickers")
+      .then((response) => response.json())
+      .then((json) => {
+        setCoins(json);
+        setLoading(false);
+      });
+  }, []);
   return (
     <div>
-      <h1> My To-dos ({Todos.length}개)</h1>
-      <form onSubmit={onSubmit}>
-        <input
-          onChange={onChange}
-          value={Todo}
-          type="text"
-          placeholder="Write your Todo"
-        ></input>
-        <button>Add To do</button>
-      </form>
+      {console.log(selectedCoin)}
+      <h1>Thc Coins!({coins.length === 0 ? "Loading" : coins.length})</h1>
+      {loading ? <strong>Loading...</strong> : null}
+      <select onChange={onSelectCoin}>
+        {coins.map((coin) => (
+          <option key={coin.id} value={coin.quotes.USD.price}>
+            {coin.name}
+          </option>
+        ))}
+      </select>
+      <br></br>
+      <label>보유 USD</label>
+      <input value={myMoney} onChange={putMyMoney} type="number"></input>
+      <label>구매가능 코인 수량</label>
+      <input value={myMoney / selectedCoin} disabled="true"></input>
     </div>
   );
 }
